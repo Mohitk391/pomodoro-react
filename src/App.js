6655c1389e5fdf746167bc67240d6e267e01adc8
Mohitk391 <mohitkpatel391@gmail.com>
2022-07-22 05:07:59 +0530

    feat: Homepage added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import { Homepage } from './pages/homepage/Homepage';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
     </div>
   );
 }
